0befe0ee512e93155f004efdb960ce05cda4d13b
Leon Linton <leon.linton@live.co.uk>
2019-05-20 12:20:46 +0100

    Refactored the methods

--- directoryV2.rb
+++ directoryV2.rb
@@ -1,28 +1,5 @@
-#Print the menu and ask the user what to do:
-def interactive_menu
-  students = []
-  loop do
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit"
-  #read the input and save it into a variable:
-  selection = gets.chomp
-  #do what the user has asked:
-    case selection
-    when "1"
-    students = input_students
-    when "2"
-    print_header
-    print(students)
-    print_footer(students)
-    when "9"
-    exit #this will cause the program to terminate
-    else
-    puts "I don't understand, please try again"
-    end
-  end
-end
-#1. Added the index+1 to print number of line
+@students = [] #an empty array accesible to all methods
+
 def input_students
   puts "Please enter the name of the student:"
   name = gets.chomp
@@ -32,14 +9,11 @@ def input_students
   country = gets.chomp
   puts "Please enter hobbie:"
   hobbie = gets.chomp
-  #create an empty array
-  students = []
-
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: cohort, country: country, hobbie: hobbie}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: cohort, country: country, hobbie: hobbie}
+    puts "Now we have #{@students.count} students"
     #gets another name from the user
     puts "Please enter the name of the student:"
     name = gets.chomp
@@ -50,8 +24,38 @@ def input_students
     puts "Please enter hobbie:"
     hobbie = gets.chomp
   end
-  #return the array of students
-students
+end
+
+def interactive_menu
+  loop do
+  print_menu
+  process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit #this will cause the program to terminate
+    else
+      puts "I don't understand, please try again"
+  end
 end
 
 def print_header
@@ -59,22 +63,16 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each_with_index do |student, index|
-#7.if students.has_value?(:cohort) false do
-
-  #if student[0] == "L"
+def print_student_list
+  @students.each_with_index do |student, index|
     puts "#{index + 1} #{student[:name]}, #{student[:country]}, #{student[:hobbie]}, (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  @students
+  puts "Overall, we have #{@students.count} great students"
 end
 
-#nothing happens until we call the methods
+#nothing happens until we call the method:
 interactive_menu
-students = input_students
-print_header
-print(students)
-print_footer(students)
