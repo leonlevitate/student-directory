6b4687c853976941ed9ba9b629b84603d22da371
Leon Linton <leon.linton@live.co.uk>
2019-05-24 10:56:14 +0100

    Added cohort verification

--- directoryV2.rb
+++ directoryV2.rb
@@ -1,35 +1,54 @@
 @students = [] #an empty array accesible to all methods
 
 def input_students
-  puts "Please enter the name of the student:"
-  name = gets.chomp
-  puts "What's your cohort month?"
-  cohort = gets.chomp
-  puts "Please enter country of birth:"
-  country = gets.chomp
-  puts "Please enter hobbie:"
-  hobbie = gets.chomp
+  puts "Please enter the name of the students:"
+  puts "To finish, just hit Enter twice"
+  name = STDIN.gets.chomp
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    @students << {name: name, cohort: cohort, country: country, hobbie: hobbie}
-    puts "Now we have #{@students.count} students"
+    @students << {
+      name: name,
+      cohort: valid_cohort?(get_cohort),
+      country: :"Evil Land",
+      hobbies: :Badminton
+    }
+    #adding variable statement for single student:
+    if @students.count == 1
+      puts "Now we have #{@students.count} student"
+    else
+      puts "Now we have #{@students.count} students"
+    end
     #gets another name from the user
-    puts "Please enter the name of the student:"
-    name = gets.chomp
-    puts "What's your cohort month?"
-    cohort = gets.chomp
-    puts "Please enter country of birth:"
-    country = gets.chomp
-    puts "Please enter hobbie:"
-    hobbie = gets.chomp
+    name = STDIN.gets.chomp
+  end
+end
+
+def get_cohort
+  puts "Please enter student's cohort (e.g July):"
+  cohort = gets.chomp.to_sym
+end
+
+def valid_cohort?(cohort)
+  cohort_options = [
+    :January, :February, :March, :April, :May, :June,
+    :July, :August, :September, :October, :November,
+    :December
+  ]
+  if cohort.empty?
+    return "November"
+  elsif cohort_options.include?(cohort)
+    return cohort
+  else
+    puts "Please check the spelling"
+    cohort = valid_cohort?(get_cohort)
   end
 end
 
 def interactive_menu
   loop do
   print_menu
-  process(gets.chomp)
+  process(STDIN.gets.chomp)
   end
 end
 
@@ -38,6 +57,7 @@ def print_menu
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
   puts "4. Load the list from students.csv"
+  puts "5. Choose a cohort to view"
   puts "9. Exit"
 end
 
@@ -59,8 +79,21 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def try_load_students
+  filename = ARGV.first #first argument from the command line
+  return if filename.nil? #get out of the method if it isn't given
+  if File.exists?(filename) #if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else #if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit #quits the program
+  end
+end
+#load_students will load from students.csv by default
+#load_students(list.txt) will load from list.txt
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
     @students << {name: name, cohort: cohort.to_sym}
@@ -78,6 +111,8 @@ def process(selection)
       save_students
     when "4"
       load_students
+    when "5"
+      print_cohort
     when "9"
       exit #this will cause the program to terminate
     else
@@ -92,14 +127,22 @@ end
 
 def print_student_list
   @students.each_with_index do |student, index|
-    puts "#{index + 1} #{student[:name]}, #{student[:country]}, #{student[:hobbie]}, (#{student[:cohort]} cohort)"
+    puts "#{index + 1} #{student[:name]} (#{student[:cohort]} cohort) #{student[:country]}, #{student[:hobbies]}".center(30)
   end
 end
 
+def print_cohort
+  puts "Which cohort would you like to view?"
+  cohort_input = STDIN.gets.chomp.to_sym
+  while cohort_input == :cohort
+  @students.map { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
+end
+end
+
 def print_footer
-  @students
   puts "Overall, we have #{@students.count} great students"
 end
 
-#nothing happens until we call the method:
+#nothing happens until we call the methods:
+try_load_students
 interactive_menu
